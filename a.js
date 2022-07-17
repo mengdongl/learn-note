6918eafeccbfbf51809dfddc1a20691df7953796
mengdongl <1589030621@qq.com>
2022-07-17 18:33:15 +0800

    init

--- a.js
+++
@@ -1,3 +0,0 @@
-exports.module={
-    a:1
-}
--- koa/my-koa-app.js
+++
@@ -1,8 +0,0 @@
-const Koa = require('koa');
-const app = new Koa();
-
-app.use(async ctx => {
-  ctx.body = 'Hello World 小老弟';
-});
-
-app.listen(3000);
